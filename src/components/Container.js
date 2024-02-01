905ab561f4a4555740215faa0a2431cfc08b960c
wendymackennah <wendymackennah5@gmail.com>
2024-02-02 01:52:19 +0300

    completed assignment

--- src/components/Container.js
+++ src/components/Container.js
@@ -9,10 +9,18 @@ function Container({
 }) {
   return (
     <div className={`container ${textPosition}`}>
+      {/* This line renders a <div> element with the class name container. The textPosition variable is used to dynamically add an additional class to the container, allowing for different text alignment (left, right, or center). */}
+
       {header ? <h2>{header}</h2> : null}
 
+      {/* This line checks if the header prop is truthy. If it is, it renders an <h2> element containing the value of the header prop; otherwise, it renders null. */}
+
       <div className={`container-children ${direction} ${contentPosition}`}>
+        {/* This line renders a nested <div> element with the class name container-children, and it includes additional classes based on the values of the direction and contentPosition variables. These classes control the layout and alignment of the child elements within the container. */}
+
         {children}
+
+        {/* This line renders the children prop, which represents the content that will be placed inside the Container component. */}
       </div>
     </div>
   );
